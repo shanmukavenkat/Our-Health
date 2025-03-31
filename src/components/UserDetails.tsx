ad162d32ddcd843eb6dac47149fc2e2ecd7129a4
shanmukavenkat <21bq1a4208@vvit.net>
2025-03-31 21:07:35 +0530

    snvskomal

--- src/components/UserDetails.tsx
+++ src/components/UserDetails.tsx
@@ -16,6 +16,7 @@ const UserDetails = () => {
 
     return (
         <>
+            
           <div className="min-h-screen bg-white text-white p-8 flex justify-center items-center">
             <div className="grid grid-cols-4 gap-6 w-full max-w-6xl">
                 {/* Full-Width Profile Image */}
@@ -26,6 +27,7 @@ const UserDetails = () => {
                 {/* User Info (Name, Email) */}
                 <div className="col-span-4 lg:col-span-2 bg-blue-900 p-6 rounded-2xl shadow-lg flex flex-col justify-center">
                 <h1 className="text-2xl font-semibold">Welcome 👋</h1>
+                <h1 className="text-lg mt-2">Hello, great to see you here!</h1>
                     <h1 className="text-2xl font-semibold">{user.firstName} {user.lastName}</h1>
                     <p className="text-gray-400 text-sm mt-2">📧{user.emailAddresses[0].emailAddress}</p>
                 </div>
