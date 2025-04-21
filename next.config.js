85c7defb7db45fd756b2abd15ce8357935e99ba6
shanmukavenkat <21bq1a4208@vvit.net>
2025-04-22 04:24:28 +0530

    snvs

--- next.config.js
+++ next.config.js
@@ -1,12 +1,9 @@
 // next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-};
-
-module.exports = nextConfig;
-module.exports = {
     reactStrictMode: true,
+    swcMinify: true,
   };
+  
+  module.exports = nextConfig;
   
